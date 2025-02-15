fcc7347afdf28404cd509590cf1b910a8b227e7e
sathmijayasuriya <sathmijayasuriya@gmail.com>
2025-02-15 15:29:10 +0530

    CHANGES

--- backend/src/main/java/com/backend/booking/constants/SQLConstants.java
+++ backend/src/main/java/com/backend/booking/constants/SQLConstants.java
@@ -1,7 +1,6 @@
 package com.backend.booking.constants;
 
 public class SQLConstants {
-    // Existing queries
     public static final String INSERT_TIME_SLOT = "INSERT INTO time_slots (date, start_time, end_time, status) VALUES (?, ?, ?, ?)";
     public static final String UPDATE_TIME_SLOT = "UPDATE time_slots SET date = ?, start_time = ?, end_time = ?, status = ? WHERE slot_id = ?";
     public static final String FIND_TIME_SLOT_BY_ID = "SELECT * FROM time_slots WHERE slot_id = ?";
