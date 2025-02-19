89ebcd3a90bc14799d0eeab0da63d2186d3e16ca
sathmijayasuriya <sathmijayasuriya@gmail.com>
2025-02-19 23:59:29 +0530

    changes

--- frontend/src/components/Menus/AdminMenu.jsx
+++ frontend/src/components/Menus/AdminMenu.jsx
@@ -62,7 +62,8 @@ export const AdminMenu = () => {
           elevation: 0,
           sx: {
             overflow: "visible",
-            filter: "drop-shadow(0px 2px 8px rgba(0,0,0,0.32))",
+            filter: "drop-shadow(0px 1px 1px rgba(139, 160, 142, 0.32))",
+            borderRadius:3,
             mt: 1.5,
            
             "& .MuiAvatar-root": {
