372110e6224920f91ea2258dbb6219bb0c56a8dd
sathmijayasuriya <sathmijayasuriya@gmail.com>
2025-02-20 18:59:46 +0530

    chore: stop tracking Configuration.js and add it to .gitignore

--- frontend/src/Configuration.js
+++
@@ -1,7 +0,0 @@
-export const Configuration = {
-    BASE_URL: "http://localhost:8080/api", // Base URL
-    USER_REGISTER: "/auth/register", // User registration endpoint
-    USER_LOGIN: "/auth/login", // User login endpoint
-    ADMIN_REGISTER: "/auth/register-admin", // Admin registration endpoint
-    // ADMIN_LOGIN: "/auth/admin/login", // Admin login endpoint
-};
