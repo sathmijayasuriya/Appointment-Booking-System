1b6dc65840ceb588614c9de0768935aa714521ba
sathmijayasuriya <sathmijayasuriya@gmail.com>
2025-02-21 13:10:59 +0530

    added test-daily-update

--- backend/src/main/java/com/backend/booking/controller/TestController.java
+++ backend/src/main/java/com/backend/booking/controller/TestController.java
@@ -2,6 +2,9 @@ package com.backend.booking.controller;
 
 
 import com.backend.booking.constants.RestURI;
+import com.backend.booking.scheduler.AppointmentScheduler;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,8 +15,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(RestURI.BASE_URL)
 public class TestController {
 
+    @Autowired
+    AppointmentScheduler appointmentScheduler;
+
     @GetMapping(RestURI.TEST)
     public String getTest(){
         return "test passed";
     }
+
+    @GetMapping("/test-daily-update")
+    public ResponseEntity<String> testDailyUpdate() {
+        appointmentScheduler.updateDailyAppointmentStatus();
+        return ResponseEntity.ok("Daily update triggered");
+    }
+
 }
